9671be47dad28297b574eba358999eef70bf381f
Thuan NN <thuan.nguyenngoc@live.com>
2017-05-28 18:12:41 +0700

    - Date time format DtFmt.cs

--- sQzLib/DtFmt.cs
+++ sQzLib/DtFmt.cs
@@ -1,26 +1,25 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Windows.Controls;
+using System.Linq;
+using System.Text;
 using System.Globalization;
-using MySql.Data.MySqlClient;
-using System.Windows.Media;
-using System.Windows;
 
 namespace sQzLib
 {
     public class DtFmt
     {
-        public static CultureInfo sCultInfo = null;
+        static CultureInfo sCultInfo = null;
         public const int INV = 0;
-        public static DateTime INV_H = DateTime.Parse("2016-01-01 00:00");//h = m = INVALID
-        public static DateTime INV_ = DateTime.Parse("2016-01-01");
-        public const string h = "HH:mm";
-        public const string H = "yyyy-MM-dd HH:mm";
-        public const string HS = "yyyy-MM-dd HH:mm:ss";
-        public const string _ = "yyyy-MM-dd";
-        public const string SH = "yy-MM-dd HH:mm";
-        public const string RH = "dd-MM-yyyy HH:mm";
-        public const string R = "dd-MM-yyyy";
+        public static DateTime INV_H = DateTime.Parse("1000-01-01 00:00:00");//h = m = INVALID
+        public static DateTime INV_ = DateTime.Parse("1000-01-01");
+        public const string h = "H:m";
+        public const string hh = "HH:mm";
+        public const string H = "yyyy-M-d H:m";
+        public const string HS = "yyyy-M-d H:m:s";
+        public const string _ = "yyyy-M-d";
+        public const string __ = "yyyy-MM-dd";
+        public const string RH = "d-M-yyyy HH:mm";
+        public const string R = "d-M-yyyy";
 
         public static bool ToDt(string s, string form, out DateTime dt)
         {
@@ -35,8 +34,8 @@ namespace sQzLib
         {
             DateTime dt;
             if (!ToDt(s, curForm, out dt))
-                return dt.ToString(H);
-            return INV_.ToString(H);
+                return dt.ToString(_);
+            return INV_.ToString(_);
         }
     }
 }
