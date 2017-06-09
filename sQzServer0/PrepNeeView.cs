df803bc9c61e954251c4673a5baf40a7f7a2495f
nauhtnn <thuan.nguyenngoc@live.com>
2017-06-09 15:52:59 +0700

    - prep0 alignment

--- sQzServer0/PrepNeeView.cs
+++ sQzServer0/PrepNeeView.cs
@@ -34,29 +34,44 @@ namespace sQzServer0
                     d.Width = cd.Width;
                     g.ColumnDefinitions.Add(d);
                 }
-                foreach (Button btn in refg.Children)
+                foreach (Button btn in refg.Children.OfType<Button>())
                 {
                     Button b = new Button();
                     b.Content = btn.Content;
                     b.Height = btn.Height;
                     b.Width = btn.Width;
                     Grid.SetColumn(b, Grid.GetColumn(btn));
-                    if (btn.Name == "x")
-                    {
-                        btnXdb = b;
-                        b.Click += btnXdb_Click;
-                    }
-                    else if (btn.Name == "a")
-                    {
-                        btnBrowse = b;
-                        b.Click += btnBrowse_Click;
-                    }
-                    else
-                    {
-                        btnXfl = b;
-                        b.Click += btnXfl_Click;
-                    }
+                    btnXdb = b;
+                    b.Click += btnXdb_Click;
                     g.Children.Add(b);
+                }
+                foreach(StackPanel sp in refg.Children.OfType<StackPanel>())
+                {
+                    StackPanel p = new StackPanel();
+                    p.Orientation = sp.Orientation;
+                    Grid.SetColumn(p, Grid.GetColumn(sp));
+                    p.HorizontalAlignment = sp.HorizontalAlignment;
+                    foreach(Button btn in sp.Children)
+                    {
+                        Button b = new Button();
+                        b.Content = btn.Content;
+                        b.Height = btn.Height;
+                        b.Width = btn.Width;
+                        b.Margin = btn.Margin;
+                        Grid.SetColumn(b, Grid.GetColumn(btn));
+                        if (btn.Name == "a")
+                        {
+                            btnBrowse = b;
+                            b.Click += btnBrowse_Click;
+                        }
+                        else
+                        {
+                            btnXfl = b;
+                            b.Click += btnXfl_Click;
+                        }
+                        p.Children.Add(b);
+                    }
+                    g.Children.Add(p);
                 }
                 g.Name = refg.Name;
                 Children.Add(g);
