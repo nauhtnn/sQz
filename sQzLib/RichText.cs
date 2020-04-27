d652be550c086b7ba748ba4eb870ea49ad049525
nauhtnn <thuan.nguyenngoc@live.com>
2020-04-28 01:16:48 +0700

    + MVC for question
    + rich text in question

---
+++ sQzLib/RichText.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sQzLib
+{
+    class RichText
+    {
+        List<object> Runs;
+
+        public bool IsTextOnly()
+        {
+            return (Runs.Count == 1) && (Runs[0].GetType() == typeof(string));
+        }
+    }
+}
