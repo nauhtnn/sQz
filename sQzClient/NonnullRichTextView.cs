69fe5999e17e5da69a0c81806933bc3c94f50f0a
nauhtnn <thuan.nguyenngoc@live.com>
2020-05-03 22:43:34 +0700

    + recursively compact stack panel
    + more readable names of variables and functions

--- sQzClient/NonnullRichTextView.cs
+++ sQzClient/NonnullRichTextView.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 using sQzLib;
+using System.Net.NetworkInformation;
 
 namespace sQzClient
 {
@@ -15,90 +16,96 @@ namespace sQzClient
         public static UIElement Render(NonnullRichText RichText)
         {
             if (RichText.HasImage())
-                return ToViewWithImage(RichText);
+                return RenderWithImage(RichText);
             else
-                return ToViewNoImage(RichText);
+                return RenderNoImage(RichText);
         }
 
-        static UIElement ToViewWithImage(NonnullRichText RichText)
+        static UIElement RenderWithImage(NonnullRichText RichText)
         {
-            StackPanel wholeViewer = new StackPanel();
-            StackPanel lineViewer = new StackPanel();
-            lineViewer.Orientation = Orientation.Horizontal;
-            Label l = new Label();
+            StackPanel lines = new StackPanel();
+            StackPanel line = new StackPanel();
+            line.Orientation = Orientation.Horizontal;
             foreach (object run in RichText.Runs)
             {
                 if (run is TextLineBreak)
                 {
-                    wholeViewer.Children.Add(lineViewer);
-                    lineViewer = new StackPanel();
-                    lineViewer.Orientation = Orientation.Horizontal;
+                    lines.Children.Add(line);
+                    line = new StackPanel();
+                    line.Orientation = Orientation.Horizontal;
                 }
                 else
-                    AddRunToLineViewer(run, lineViewer);
+                    AddTextOrImageToLine(run, line);
             }
-            if (lineViewer.Children.Count > 0)
-                wholeViewer.Children.Add(lineViewer);
-            return CompactStack(wholeViewer);
+            if (line.Children.Count > 0)
+                lines.Children.Add(line);
+            return CompactStack(lines);
         }
 
-        static StackPanel CompactStack(StackPanel stack)
+        static UIElement CompactStack(UIElement source)
         {
-            StackPanel compactStack = new StackPanel();
-            foreach(UIElement i in stack.Children)
-            {
-                StackPanel childStack = i as StackPanel;
-                if(childStack == null)
-                    compactStack.Children.Add(childStack);
-                else
-                {
-                    if (childStack.Children.Count == 1)
-                    {
-                        UIElement element = childStack.Children[0];
-                        childStack.Children.Remove(element);
-                        compactStack.Children.Add(element);
-                    }
-                    else if(childStack.Children.Count > 1)
-                    {
-                        UIElement[] elements = new UIElement[childStack.Children.Count];
-                        childStack.Children.CopyTo(elements, 0);
-                        childStack.Children.Clear();
-                        StackPanel cloneChildStack = new StackPanel();
-                        cloneChildStack.Orientation = childStack.Orientation;
-                        foreach (UIElement element in elements)
-                            cloneChildStack.Children.Add(element);
-                        compactStack.Children.Add(cloneChildStack);
-                    }
-                }
-            }
-            return compactStack;
+            StackPanel IsStack = source as StackPanel;
+            if (IsStack == null)
+                return source;
+            else if (IsStack.Children.Count == 1)
+                return CompactStackHaving1Child(IsStack);
+            else
+                return CompactStackHavingChildren(IsStack);
         }
 
-        static void AddRunToLineViewer(object run, StackPanel lineViewer)
+        static UIElement CompactStackHaving1Child(StackPanel source)
+        {
+            UIElement onlyChild = source.Children[0];
+            source.Children.Clear();
+            return CompactStack(onlyChild);
+        }
+
+        static StackPanel CompactStackHavingChildren(StackPanel source)
+        {
+            StackPanel target = new StackPanel();
+            target.Orientation = source.Orientation;
+            UIElement[] children = GetChildrenFromStackPanel(source);
+            foreach (UIElement child in children)
+                target.Children.Add(CompactStack(child));
+            return target;
+        }
+
+        static UIElement[] GetChildrenFromStackPanel(StackPanel source)
+        {
+            UIElement[] children = new UIElement[source.Children.Count];
+            source.Children.CopyTo(children, 0);
+            source.Children.Clear();
+            return children;
+        }
+
+        static void AddTextOrImageToLine(object run, StackPanel line)
         {
             string s = run as string;
             if (s == null)
-            {
-                BitmapImage bi = new BitmapImage();
-                var stream = new MemoryStream(run as byte[]);
-                bi.BeginInit();
-                bi.StreamSource = stream;
-                bi.EndInit();
-                Image img = new Image();
-                img.Source = bi;
-                img.Width = bi.Width;
-                img.Height = bi.Height;
-                lineViewer.Children.Add(img);
-            }
+                AddImageToLine(run as byte[], line);
             else
             {
                 TextBlock t = new TextBlock();
                 t.Text = s;
-                lineViewer.Children.Add(t);
+                line.Children.Add(t);
             }
         }
 
-        static UIElement ToViewNoImage(NonnullRichText RichText)
+        static void AddImageToLine(byte[] imageInBytes, StackPanel line)
+        {
+            BitmapImage bi = new BitmapImage();
+            var stream = new MemoryStream(imageInBytes);
+            bi.BeginInit();
+            bi.StreamSource = stream;
+            bi.EndInit();
+            Image img = new Image();
+            img.Source = bi;
+            img.Width = bi.Width;
+            img.Height = bi.Height;
+            line.Children.Add(img);
+        }
+
+        static UIElement RenderNoImage(NonnullRichText RichText)
         {
             StringBuilder text = new StringBuilder();
             foreach (object run in RichText.Runs)
