2708a71d5142f3f1d173a6c6a635c52de7460f5d
nauhtnn <thuan.nguyennngoc@live.com>
2020-05-03 10:22:33 +0700

    + new class to mark line break, and stack panel contains child stack panel for multiple lines

--- sQzClient/NonnullRichTextView.cs
+++ sQzClient/NonnullRichTextView.cs
@@ -22,31 +22,80 @@ namespace sQzClient
 
         static UIElement ToViewWithImage(NonnullRichText RichText)
         {
-            StackPanel content = new StackPanel();
+            StackPanel wholeViewer = new StackPanel();
+            StackPanel lineViewer = new StackPanel();
+            lineViewer.Orientation = Orientation.Horizontal;
+            Label l = new Label();
             foreach (object run in RichText.Runs)
             {
-                string s = run as string;
-                if (s == null)
+                if (run is TextLineBreak)
                 {
-                    BitmapImage bi = new BitmapImage();
-                    var stream = new MemoryStream(run as byte[]);
-                    bi.BeginInit();
-                    bi.StreamSource = stream;
-                    bi.EndInit();
-                    Image img = new Image();
-                    img.Source = bi;
-                    img.Width = bi.Width;
-                    img.Height = bi.Height;
-                    content.Children.Add(img);
+                    wholeViewer.Children.Add(lineViewer);
+                    lineViewer = new StackPanel();
+                    lineViewer.Orientation = Orientation.Horizontal;
                 }
                 else
+                    AddRunToLineViewer(run, lineViewer);
+            }
+            if (lineViewer.Children.Count > 0)
+                wholeViewer.Children.Add(lineViewer);
+            return CompactStack(wholeViewer);
+        }
+
+        static StackPanel CompactStack(StackPanel stack)
+        {
+            StackPanel compactStack = new StackPanel();
+            foreach(UIElement i in stack.Children)
+            {
+                StackPanel childStack = i as StackPanel;
+                if(childStack == null)
+                    compactStack.Children.Add(childStack);
+                else
                 {
-                    TextBlock t = new TextBlock();
-                    t.Text = s;
-                    content.Children.Add(t);
+                    if (childStack.Children.Count == 1)
+                    {
+                        UIElement element = childStack.Children[0];
+                        childStack.Children.Remove(element);
+                        compactStack.Children.Add(element);
+                    }
+                    else if(childStack.Children.Count > 1)
+                    {
+                        UIElement[] elements = new UIElement[childStack.Children.Count];
+                        childStack.Children.CopyTo(elements, 0);
+                        childStack.Children.Clear();
+                        StackPanel cloneChildStack = new StackPanel();
+                        cloneChildStack.Orientation = childStack.Orientation;
+                        foreach (UIElement element in elements)
+                            cloneChildStack.Children.Add(element);
+                        compactStack.Children.Add(cloneChildStack);
+                    }
                 }
             }
-            return content;
+            return compactStack;
+        }
+
+        static void AddRunToLineViewer(object run, StackPanel lineViewer)
+        {
+            string s = run as string;
+            if (s == null)
+            {
+                BitmapImage bi = new BitmapImage();
+                var stream = new MemoryStream(run as byte[]);
+                bi.BeginInit();
+                bi.StreamSource = stream;
+                bi.EndInit();
+                Image img = new Image();
+                img.Source = bi;
+                img.Width = bi.Width;
+                img.Height = bi.Height;
+                lineViewer.Children.Add(img);
+            }
+            else
+            {
+                TextBlock t = new TextBlock();
+                t.Text = s;
+                lineViewer.Children.Add(t);
+            }
         }
 
         static UIElement ToViewNoImage(NonnullRichText RichText)
---
+++ sQzLib/TextLineBreak.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sQzLib
+{
+    public class TextLineBreak
+    {
+        public override string ToString()
+        {
+            return "This class is to mark new line. It is used by viewer when rendering.";
+        }
+    }
+}
